ae2817735a340866ee585ea06a115e5153e395c5
Anayn Pavez <nanyn.pavez@gmail.com>
2017-05-05 14:01:56 -0300

    version con el recorrido correcto del arreglo hobbies e impresión en html

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-function MiembroSquad(nombre, edad, hobbies){
+function MiembroSquad(nombre, edad, hobbies, id){
 	this.nombre = nombre;
 	this.edad = edad;
 	this.hobbies = hobbies;
@@ -6,60 +6,48 @@ function MiembroSquad(nombre, edad, hobbies){
 
 var arrSquad = [];
 
-var josefa = new MiembroSquad("Josefa Herrera", 20,['bailar pop', 'ver series', 'codear']);
+var josefa = new MiembroSquad("Josefa Herrera", 20,['bailar pop', 'ver series', 'codear'], 'miembro0');
 arrSquad.push(josefa);
 
-arrSquad.forEach(function(element){
-	return element.hobbies;
-
-	element.hobbies.forEach(function(e){
-		lista.innerHTML += "<li>" + e + "</li>";
-	})
-})
-
-var mai = new MiembroSquad('Maibeet Torres', 14, ['juegos rpg', 'leer', 'andar en bici']);
+var mai = new MiembroSquad('Maibeet Torres', 14, ['juegos rpg', 'leer', 'andar en bici'], 'miembro1');
 arrSquad.push(mai);
 
-var caro = new MiembroSquad('Carolina Quintana', 31, ['patinar', 'comer sushi','acariciar a mi gata']);
+var caro = new MiembroSquad('Carolina Quintana', 31, ['patinar', 'comer sushi','acariciar a mi gata'], 'miembro2');
 arrSquad.push(caro);
 
-var anais = new MiembroSquad('Anais ', 20, ['estar con la amelia', 'ver netflix', 'pololiar']);
+var anais = new MiembroSquad('Anais Araya ', 20, ['estar con la amelia', 'ver netflix', 'pololiar'], 'miembro3');
 arrSquad.push(anais);
 
-var muri = new MiembroSquad('Muriel Bahamondes', 25, ['fumar hierba', 'jugar pool', 'navegar en internet']);
+var muri = new MiembroSquad('Muriel Bahamondes', 25, ['fumar hierba', 'jugar pool', 'navegar en internet'], 'miembro4');
 arrSquad.push(muri);
 
-var simone = new MiembroSquad('Simona Ulloa', 29,['ver netflix', 'escuchar música', 'tomar chela']);
+var simone = new MiembroSquad('Simona Ulloa', 29,['ver netflix', 'escuchar música', 'tomar chela'], 'miembro5');
 arrSquad.push(simone);
 
 var sandra; 
 
-var dulce = new MiembroSquad('Dulce', 15, ['cocinar', 'leer', 'subir montañas']);
+var dulce = new MiembroSquad('Dulce', 15, ['cocinar', 'leer', 'subir montañas'], 'miembro6');
 arrSquad.push(dulce);
 
-
-
-
-var esp = document.getElementById('espacio');
 
 var lista =document.getElementById('integrante');
 
 arrSquad.forEach(function(element){
-	return element.hobbies;
+	lista.innerHTML += '<br>Nombre: '+ element.nombre + '<br>Edad: '+ element.edad + '<br>Hobbies: ' + '<ul>';
 
 	element.hobbies.forEach(function(e){
-		lista.innerHTML += "<li>" + e + "</li>";
+	lista.innerHTML += "<li>" + e + "</li>";
 	})
+	lista.innerHTML += '</ul>';
 })
 
-
-esp.innerHTML += '<br>Nombre: '+ josefa.nombre + '<br>Edad: '+ josefa.edad + '<br>Hobbies: ' + josefa.hobbies+'<br>';
-esp.innerHTML += '<br>Nombre: '+ mai.nombre + '<br>Edad: '+ mai.edad + '<br>Hobbies: ' + mai.hobbies+'<br>';
-esp.innerHTML += '<br>Nombre: '+ caro.nombre + '<br>Edad: '+ caro.edad + '<br>Hobbies: ' + caro.hobbies+'<br>';
-esp.innerHTML += '<br>Nombre: '+ anais.nombre + '<br>Edad: '+ anais.edad + '<br>Hobbies: ' + anais.hobbies+'<br>';
-esp.innerHTML += '<br>Nombre: '+ muri.nombre + '<br>Edad: '+ muri.edad + '<br>Hobbies: ' + muri.hobbies+'<br>';
-esp.innerHTML += '<br>Nombre: '+ simone.nombre + '<br>Edad: '+ simone.edad + '<br>Hobbies: ' + simone.hobbies+'<br>';
-esp.innerHTML += '<br>Nombre: '+ dulce.nombre + '<br>Edad: '+ dulce.edad + '<br>Hobbies: ' + dulce.hobbies+'<br>';
+/*lista.innerHTML += '<br>Nombre: '+ josefa.nombre + '<br>Edad: '+ josefa.edad + '<br>Hobbies: ' + josefa.hobbies+'<br>';
+lista.innerHTML += '<br>Nombre: '+ mai.nombre + '<br>Edad: '+ mai.edad + '<br>Hobbies: ' + mai.hobbies+'<br>';
+lista.innerHTML += '<br>Nombre: '+ caro.nombre + '<br>Edad: '+ caro.edad + '<br>Hobbies: ' + caro.hobbies+'<br>';
+lista.innerHTML += '<br>Nombre: '+ anais.nombre + '<br>Edad: '+ anais.edad + '<br>Hobbies: ' + anais.hobbies+'<br>';
+lista.innerHTML += '<br>Nombre: '+ muri.nombre + '<br>Edad: '+ muri.edad + '<br>Hobbies: ' + muri.hobbies+'<br>';
+lista.innerHTML += '<br>Nombre: '+ simone.nombre + '<br>Edad: '+ simone.edad + '<br>Hobbies: ' + simone.hobbies+'<br>';
+lista.innerHTML += '<br>Nombre: '+ dulce.nombre + '<br>Edad: '+ dulce.edad + '<br>Hobbies: ' + dulce.hobbies+'<br>';*/
 
 
 
