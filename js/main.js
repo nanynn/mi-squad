248efe24ade6a24cfcdb83c3ed067d5102d5127a
Anayn Pavez <nanyn.pavez@gmail.com>
2017-05-07 22:56:33 -0300

    version con todas los espacios creados en html desde js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-function MiembroSquad(nombre, edad, hobbies, id){
+function MiembroSquad(nombre, edad, hobbies){
 	this.nombre = nombre;
 	this.edad = edad;
 	this.hobbies = hobbies;
@@ -6,33 +6,35 @@ function MiembroSquad(nombre, edad, hobbies, id){
 
 var arrSquad = [];
 
-var josefa = new MiembroSquad("Josefa Herrera", 20,['bailar pop', 'ver series', 'codear'], 'miembro0');
+//cada miembro del squad sumado de a uno 
+var josefa = new MiembroSquad("Josefa Herrera", 20,['bailar pop', 'ver series', 'codear']);
 arrSquad.push(josefa);
 
-var mai = new MiembroSquad('Maibeet Torres', 14, ['juegos rpg', 'leer', 'andar en bici'], 'miembro1');
+var mai = new MiembroSquad('Maibeet Torres', 14, ['juegos rpg', 'leer', 'andar en bici']);
 arrSquad.push(mai);
 
-var caro = new MiembroSquad('Carolina Quintana', 31, ['patinar', 'comer sushi','acariciar a mi gata'], 'miembro2');
+var caro = new MiembroSquad('Carolina Quintana', 31, ['patinar', 'comer sushi','acariciar a mi gata']);
 arrSquad.push(caro);
 
-var anais = new MiembroSquad('Anais Araya ', 20, ['estar con la amelia', 'ver netflix', 'pololiar'], 'miembro3');
+var anais = new MiembroSquad('Anais Araya ', 20, ['estar con la amelia', 'ver netflix', 'pololiar']);
 arrSquad.push(anais);
 
-var muri = new MiembroSquad('Muriel Bahamondes', 25, ['fumar hierba', 'jugar pool', 'navegar en internet'], 'miembro4');
+var muri = new MiembroSquad('Muriel Bahamondes', 25, ['fumar hierba', 'jugar pool', 'navegar en internet']);
 arrSquad.push(muri);
 
-var simone = new MiembroSquad('Simona Ulloa', 29,['ver netflix', 'escuchar música', 'tomar chela'], 'miembro5');
+var simone = new MiembroSquad('Simona Ulloa', 29,['ver netflix', 'escuchar música', 'tomar chela']);
 arrSquad.push(simone);
 
-var sandra; 
-
-var dulce = new MiembroSquad('Dulce Frau', 21, ['cocinar', 'leer', 'subir montañas'], 'miembro6');
+var dulce = new MiembroSquad('Dulce Frau', 21, ['cocinar', 'leer', 'subir montañas']);
 arrSquad.push(dulce);
 
+var sandra; //no dió sus datos
+
+function mostrarDatos(){
 
 var lista =document.getElementById('integrante');
 
-arrSquad.forEach(function(element){
+	arrSquad.forEach(function(element){
 	var squad = document.createElement('div');
 	squad.innerHTML += '<br>Nombre: '+ element.nombre + '<br>Edad: '+ element.edad + '<br>Hobbies: ';
 
@@ -42,8 +44,46 @@ arrSquad.forEach(function(element){
 			})
 		squad.appendChild(lis);
 
+		var coment = document.createElement('textarea');
+		coment.class="palabras";
+		squad.appendChild(coment);
+
+		var boton = document.createElement('button');
+		boton.onclick="dejarComent()";
+		boton.innerHTML += 'Agregar comentario';
+		squad.appendChild(boton);
+
+		var corazon = document.createElement('button');
+		corazon.innerHTML += '<i class="fa fa-heart" aria-hidden="true"></i>';
+		squad.appendChild(corazon);
+
+		var espacioComentario = document.createElement('div');
+		squad.appendChild(espacioComentario);
+
+		function dejarComent(){
+			var comentarios = document.getElementsByClassName('palabras');
+			var espacioComentario = document.getElementById('div');
+			comentarios[0].innerHTML += espacioComentario.value;
+			comentarios.value = "";
+		}
+
 	lista.appendChild(squad);
-})
+	})
+}
+mostrarDatos();
+
+
+
+
+function sumarLike(){
+
+}
+
+function Comentarios(id,comentario,likes){
+	this.id = id;
+	this.comentario = comentario;
+	this.like = likes;
+}
 
 /*lista.innerHTML += '<br>Nombre: '+ josefa.nombre + '<br>Edad: '+ josefa.edad + '<br>Hobbies: ' + josefa.hobbies+'<br>';
 lista.innerHTML += '<br>Nombre: '+ mai.nombre + '<br>Edad: '+ mai.edad + '<br>Hobbies: ' + mai.hobbies+'<br>';
@@ -53,7 +93,18 @@ lista.innerHTML += '<br>Nombre: '+ muri.nombre + '<br>Edad: '+ muri.edad + '<br>
 lista.innerHTML += '<br>Nombre: '+ simone.nombre + '<br>Edad: '+ simone.edad + '<br>Hobbies: ' + simone.hobbies+'<br>';
 lista.innerHTML += '<br>Nombre: '+ dulce.nombre + '<br>Edad: '+ dulce.edad + '<br>Hobbies: ' + dulce.hobbies+'<br>';*/
 
-//function Comentarios(id, ){}
+/*	<label for="nuevo">Añadir una nueva tarea</label>
+	<input type="nuevo" id="nuevo">
+
+	<button id="boton" onclick="botonFuncional()">Añadir</button>
+
+	 function botonFuncional() {
+ -	var input = document.getElementById('nuevo').value;
+ +	var parametro = document.getElementById('nuevo').value;
+  	var informacion = document.getElementsByClassName('anadir-tarea');
+ -	informacion[0].innerHTML += "<li>" + input + "</li>";
+ -}*/
+
 
 
 
